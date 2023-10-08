ed4c51f0ef8c85173f1e57a4942ef7557c947b1a
Harriet O'Brien <hobrien@alumni.cmu.edu>
2023-10-08 22:36:49 +0000

    annoyed

--- cpsubmods.sh
+++ cpsubmods.sh
@@ -1,5 +1,5 @@
 AUR="https://aur.archlinux.org/"
 git clone ${AUR}cryptmount.git cryptmount-cp \
-  && rm -rf cryptmount/.git
+  && rm -rf cryptmount-cp/.git
 git clone ${AUR}krita-beta-appimage.git krita-beta-appimage-cp \
-  && rm -rf krita-beta-appimage/.git
+  && rm -rf krita-beta-appimage-cp/.git
